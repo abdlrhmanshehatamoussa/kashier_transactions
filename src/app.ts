d029f129381079302b6f532353b7445e8a886392
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2022-01-27 20:41:04 +0200

    Adding docker

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,9 @@ import express from 'express';
 import addRoutes from './router';
 
 const app = express();
-const port = 3000;
+const port = 8080;
 const bodyParser = require('body-parser')
 app.use(bodyParser.json({ limit: '50mb' }));
-app.use('/api-docs/swagger', express.static('swagger'));
-app.use('/api-docs/swagger/assets', express.static('node_modules/swagger-ui-dist'));
 
 addRoutes(app)
 

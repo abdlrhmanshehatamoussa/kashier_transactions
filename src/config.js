b9588c20f3f636ab54c0390831a95dc1266a4000
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2022-01-29 16:45:01 +0200

    minor changes

--- src/config.js
+++ src/config.js
@@ -4,6 +4,8 @@ const utils = require('./commons/utils');
 const apiPort = 8080;
 const databaseConnectionString = process.env.DB_URL;
 const databaseName = process.env.DB_NAME;
+const apiVersion = '1.0.0'
+const apiEnv = process.env.API_ENV
 
 if (
     utils.isNullOrEmpty(databaseConnectionString) ||
@@ -16,5 +18,7 @@ if (
 module.exports = {
     apiPort,
     databaseConnectionString,
-    databaseName
+    databaseName,
+    apiVersion,
+    apiEnv
 }
--- src/endpoints/ping/ping.controller.js
+++ src/endpoints/ping/ping.controller.js
@@ -1,8 +1,11 @@
 const utils = require('../../commons/utils');
+const { apiVersion, apiEnv } = require('../../config')
 
 const ping = utils.catchAsync(async(req, res) => {
     res.status(200).json({
         message: "Welcome to Kashier Transactions Service.",
+        environment: apiEnv,
+        version: apiVersion,
         timestamp: (new Date).toISOString()
     });
 });

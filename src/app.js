de1b120a460abdbb48e0d6cd638d25e29e08f822
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2022-01-29 18:09:42 +0200

    Adding swagger

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ const app = express();
 const bodyParser = require('body-parser')
 const router = require('./router');
 const errorHandler = require('./commons/error_middleware');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger')
 
 app.use(bodyParser.json());
 app.use(router)
 app.use(errorHandler)
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 module.exports = app;
--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const utils = require('./commons/utils');
 const apiPort = 8080;
 const databaseConnectionString = process.env.DB_URL;
 const databaseName = process.env.DB_NAME;
-const apiVersion = '1.0.0'
+const apiVersion = '1.0.1'
 const apiEnv = process.env.API_ENV
 
 if (
